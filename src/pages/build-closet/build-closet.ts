b9abf49a71387aad94768562f133151bb8cd43e0
Markous Soliman <markous.soliman@gmail.com>
2016-12-17 00:07:37 -0500

    feature/build-closet: Added shirts questions & color chooser

--- src/pages/build-closet/build-closet.ts
+++ src/pages/build-closet/build-closet.ts
@@ -13,7 +13,44 @@ import { NavController } from 'ionic-angular';
 })
 export class BuildCloset {
 
-  constructor(public navCtrl: NavController) {}
+	selectedShirtType: number;
+	selectedShirtTypeImage: string;
+	shirtTypes: any[];
+	colors: any[];
+	selectedColor: string;
+
+
+	constructor(public navCtrl: NavController) {
+		this.selectedShirtTypeImage = "../assets/img/t-shirt.png";
+		this.selectedShirtType = 0;
+		this.shirtTypes = [{id: 1, type: 'T-Shirt'},
+			{id: 2, type: 'Classic'},
+			{id: 3, type: 'Polo'},
+			{id: 4, type: 'Henley'},
+			{id: 5, type: 'Turtleneck'},
+			{id: 6, type: 'Sweathshirt'},
+			{id: 7, type: 'Sweater'}];
+
+		this.colors = [{id: 1, hex: "#FFFFFF", name: 'White'},
+			{id: 2, hex: "#F5F5DC", name: 'Beige'},
+			{id: 3, hex: "#FFC0CB", name: 'Pink'},
+			{id: 4, hex: "#FF0000", name: 'Red'},
+			{id: 5, hex: "#FFA500", name: 'Orange'},
+			{id: 6, hex: "#FFFF00", name: 'Yellow'},
+			{id: 7, hex: "#008000", name: 'Green'},
+			{id: 8, hex: "#008080", name: 'Teal'},
+			{id: 9, hex: "#448BFC", name: 'Blue'},
+			{id: 10, hex: "#800080", name: 'Purple'},
+			{id: 11, hex: "#85491E", name: 'Brown'},
+			{id: 12, hex: "#808080", name: 'Grey'},
+			{id: 13, hex: "#000000", name: 'Black'}];
+
+
+ 	}
+
+ 	selectColor(color) {
+ 		this.selectedColor = color.hex;
+ 	}
 
   ionViewDidLoad() {
     //console.log('Hello BuildClosetPage Page');
